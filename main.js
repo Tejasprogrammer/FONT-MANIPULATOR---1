0a9efd17717bb65851df513f59211cd82d044558
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-07-24 14:15:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
-function setup()
-{
+leftWrist_x = 0;
+rightWrist_x = 0;
+difference = 0;
+
+function setup(){
     video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
 
-    video.size(550 , 500);
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
 
-    canvas=createCanvas(550 , 500 );
-    canvas.position(550, 150)
-
-    poseNet = ml5.poseNet(video , modelLoaded);
-    poseNet.on('Pose' , gotPoses);
-
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
 }
 
-function modelLoaded()
-{
-    console.log("PoseNet is Initilished");
+function draw(){
+    background("#5196e3");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#00ff0a");
+    text('Tejas Patil',50,300);
 }
 
-function gotPoses(results)
-{
- if(results.length > 0)
- {
-    console.log(results);
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
 
- }
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist_x - rightWrist_x);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
 }
